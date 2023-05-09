f2527d461b4ab0201f34d43416fce95783d3ba6f
glopesdev <glopesdev@users.noreply.github.com>
2023-05-09 15:55:35 +0100

    Add first draft of breakout board hub device

---
+++ OpenEphys.Onix/OpenEphys.Onix/BreakoutBoard.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Xml.Serialization;
+using Bonsai;
+
+namespace OpenEphys.Onix
+{
+    [TypeDescriptionProvider(typeof(DeviceHubTypeDescriptionProvider<BreakoutBoard>))]
+    public class BreakoutBoard : DeviceFactory, INamedElement
+    {
+        string name;
+
+        public BreakoutBoard() { Name = null; }
+
+        [XmlArrayItem(typeof(ConfigureHeartbeat))]
+        [XmlArrayItem(typeof(ConfigureAnalogIO))]
+        public List<SingleDeviceFactory> Devices = new List<SingleDeviceFactory>
+        {
+            new ConfigureHeartbeat { DeviceIndex = 0 },
+            new ConfigureAnalogIO { DeviceIndex = 6 }
+        };
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                var baseName = string.IsNullOrWhiteSpace(name) ? nameof(BreakoutBoard) : name;
+                foreach (var device in Devices)
+                {
+                    device.DeviceName = $"{baseName}/{device.DeviceType.Name}";
+                }
+            }
+        }
+
+        internal override IEnumerable<IDeviceConfiguration> GetDevices()
+        {
+            return Devices;
+        }
+
+        public override IObservable<ContextTask> Process(IObservable<ContextTask> source)
+        {
+            var output = source;
+            foreach (var device in Devices)
+            {
+                output = device.Process(output);
+            }
+
+            return output;
+        }
+    }
+}
---
+++ OpenEphys.Onix/OpenEphys.Onix/DeviceHubTypeDescriptionProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace OpenEphys.Onix
+{
+    class DeviceHubTypeDescriptionProvider<THub> : TypeDescriptionProvider where THub : DeviceFactory
+    {
+        static readonly TypeDescriptionProvider BaseProvider = TypeDescriptor.GetProvider(typeof(THub));
+
+        public DeviceHubTypeDescriptionProvider()
+            : base(BaseProvider)
+        {
+        }
+
+        public override ICustomTypeDescriptor GetExtendedTypeDescriptor(object instance)
+        {
+            var hub = (DeviceFactory)instance;
+            if (hub != null)
+            {
+                return new DeviceHubTypeDescriptor(hub);
+            }
+
+            return base.GetExtendedTypeDescriptor(instance);
+        }
+    }
+}
