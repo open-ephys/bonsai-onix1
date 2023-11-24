6d3ffcd98b52966f5f6d4e00ed739295cc7ce683
glopesdev <glopesdev@users.noreply.github.com>
2023-11-24 09:22:59 +0000

    Add RHD2164 registers and configure data format

--- OpenEphys.Onix/OpenEphys.Onix/ConfigureRhd2164.cs
+++ OpenEphys.Onix/OpenEphys.Onix/ConfigureRhd2164.cs
@@ -14,6 +14,12 @@ namespace OpenEphys.Onix
         [Description("Specifies whether the RHD2164 device is enabled.")]
         public bool Enable { get; set; } = true;
 
+        [Category(ConfigurationCategory)]
+        public Rhd2164AmplifierDataFormat AmplifierDataFormat { get; set; }
+
+        [Category(ConfigurationCategory)]
+        public Rhd2164DspCutoff DspCutoff { get; set; }
+
         public override IObservable<ContextTask> Process(IObservable<ContextTask> source)
         {
             var enable = Enable;
@@ -21,8 +27,23 @@ namespace OpenEphys.Onix
             var deviceAddress = DeviceAddress;
             return source.ConfigureDevice(context =>
             {
+                // config register format following RHD2164 datasheet
+                // https://intantech.com/files/Intan_RHD2000_series_datasheet.pdf
                 var device = context.GetDevice(deviceAddress, Rhd2164.ID);
                 context.WriteRegister(deviceAddress, Rhd2164.ENABLE, enable ? 1u : 0);
+
+                var format = 0;
+                var amplifierDataFormat = AmplifierDataFormat;
+                format |= (int)amplifierDataFormat << 6;
+
+                var dspCutoff = DspCutoff;
+                if (dspCutoff != Rhd2164DspCutoff.Off)
+                {
+                    format |= 1 << 4;
+                    format |= (int)dspCutoff;
+                }
+
+                context.WriteRegister(deviceAddress, Rhd2164.FORMAT, (uint)format);
 
                 var deviceInfo = new DeviceInfo(context, DeviceType, deviceAddress);
                 var disposable = DeviceManager.RegisterDevice(deviceName, deviceInfo);
@@ -36,6 +57,30 @@ namespace OpenEphys.Onix
         public const int ID = 3;
 
         public const uint ENABLE = 0x10000;  // Enable the heartbeat
+
+        // unmanaged registers
+        public const uint ADCCONF = 0x00; // ADC Configuration and Amplifier Fast Settle
+        public const uint ADCBUFF = 0x01; // Supply Sensor and ADC Buffer Bias Current
+        public const uint MUXBIAS = 0x02; // MUX Bias Current
+        public const uint MUXLOAD = 0x03; // MUX Load, Temperature Sensor, and Auxiliary Digital Output
+        public const uint FORMAT = 0x04; // ADC Output Format and DSP Offset Removal
+        public const uint ZCHECK = 0x05; // Impedance Check Control
+        public const uint ZDAC = 0x06; // Impedance Check DAC
+        public const uint ZSELECT = 0x07; // Impedance Check Amplifier Select
+        public const uint BW0 = 0x08; // On-Chip Amplifier Bandwidth Select 0
+        public const uint BW1 = 0x09; // On-Chip Amplifier Bandwidth Select 1
+        public const uint BW2 = 0x0a; // On-Chip Amplifier Bandwidth Select 2
+        public const uint BW3 = 0x0b; // On-Chip Amplifier Bandwidth Select 3
+        public const uint BW4 = 0x0c; // On-Chip Amplifier Bandwidth Select 4
+        public const uint BW5 = 0x0e; // On-Chip Amplifier Bandwidth Select 5
+        public const uint PWR0 = 0x0f; // Individual Amplifier Power 0
+        public const uint PWR1 = 0x10; // Individual Amplifier Power 1
+        public const uint PWR2 = 0x11; // Individual Amplifier Power 2
+        public const uint PWR3 = 0x12; // Individual Amplifier Power 3
+        public const uint PWR4 = 0x13; // Individual Amplifier Power 4
+        public const uint PWR5 = 0x14; // Individual Amplifier Power 5
+        public const uint PWR6 = 0x15; // Individual Amplifier Power 6
+        public const uint PWR7 = 0x16; // Individual Amplifier Power 7
 
         internal class NameConverter : DeviceNameConverter
         {
---
+++ OpenEphys.Onix/OpenEphys.Onix/Rhd2164Config.cs
@@ -0,0 +1,96 @@
+﻿namespace OpenEphys.Onix
+{
+    public enum Rhd2164DspCutoff
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Differential = 0,
+
+        /// <summary>
+        /// 3310 Hz
+        /// </summary>
+        Dsp3309Hz,
+
+        /// <summary>
+        /// 1370 Hz
+        /// </summary>
+        Dsp1374Hz,
+        
+        /// <summary>
+        /// 638 Hz
+        /// </summary>
+        Dsp638Hz,
+        
+        /// <summary>
+        /// 308 Hz
+        /// </summary>
+        Dsp308Hz,
+        
+        /// <summary>
+        /// 152 Hz
+        /// </summary>
+        Dsp152Hz,
+        
+        /// <summary>
+        /// 75.2 Hz
+        /// </summary>
+        Dsp75Hz,
+        
+        /// <summary>
+        /// 37.4 Hz
+        /// </summary>
+        Dsp37Hz,
+        
+        /// <summary>
+        /// 18.7 Hz
+        /// </summary>
+        Dsp19Hz,
+        
+        /// <summary>
+        /// 9.34 Hz
+        /// </summary>
+        Dsp9336mHz,
+        
+        /// <summary>
+        /// 4.67 Hz
+        /// </summary>
+        Dsp4665mHz,
+        
+        /// <summary>
+        /// 2.33 Hz
+        /// </summary>
+        Dsp2332mHz,
+        
+        /// <summary>
+        /// 1.17 Hz
+        /// </summary>
+        Dsp1166mHz,
+        
+        /// <summary>
+        /// 0.583 Hz
+        /// </summary>
+        Dsp583mHz,
+        
+        /// <summary>
+        /// 0.291 Hz
+        /// </summary>
+        Dsp291mHz,
+        
+        /// <summary>
+        /// 0.146 Hz
+        /// </summary>
+        Dsp146mHz,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        Off
+    }
+
+    public enum Rhd2164AmplifierDataFormat
+    {
+        Unsigned,
+        TwosComplement
+    }
+}
