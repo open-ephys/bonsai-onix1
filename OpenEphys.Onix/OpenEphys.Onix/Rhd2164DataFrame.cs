aafc71027111bbf2343dd9bd2bdefb9d6c59c4a4
glopesdev <glopesdev@users.noreply.github.com>
2023-11-28 22:39:36 +0000

    Add clock buffers to RHD2164 data frame

--- OpenEphys.Onix/OpenEphys.Onix/Rhd2164DataFrame.cs
+++ OpenEphys.Onix/OpenEphys.Onix/Rhd2164DataFrame.cs
@@ -1,22 +1,21 @@
-﻿using System.Net;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using OpenCV.Net;
 
 namespace OpenEphys.Onix
 {
     public class Rhd2164DataFrame
     {
-        public Rhd2164DataFrame(ulong clock, long hubClock, Mat amplifierData, Mat auxData)
+        public Rhd2164DataFrame(ulong[] clock, ulong[] hubClock, Mat amplifierData, Mat auxData)
         {
             Clock = clock;
-            HubClock = unchecked((ulong)IPAddress.NetworkToHostOrder(hubClock));
+            HubClock = hubClock;
             AmplifierData = amplifierData;
             AuxData = auxData;
         }
 
-        public ulong Clock { get; }
+        public ulong[] Clock { get; }
 
-        public ulong HubClock { get; }
+        public ulong[] HubClock { get; }
 
         public Mat AmplifierData { get; }
 
@@ -26,7 +25,7 @@ namespace OpenEphys.Onix
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     unsafe struct Rhd2164Payload
     {
-        public long HubClock;
+        public ulong HubClock;
         public fixed ushort AmplifierData[Rhd2164.AmplifierChannelCount];
         public fixed ushort AuxData[Rhd2164.AuxChannelCount];
     }
