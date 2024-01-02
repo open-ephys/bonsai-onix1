2548141d8268e07f4386e0183efc37138bd4b0ed
jonnew <jpn@open-ephys.org>
2024-01-02 16:08:27 -0500

    Add support fort Test0 device
    - This device is documented here
    https://open-ephys.github.io/onix-docs/Hardware%20Guide/Datasheets/test-0.html
    - It has register-settable frame size and is the sole device in the
    table presented by the onidriver_test hardware emulator.
    - This commit is a request for review, namely in dealing with the
    Test0DataFrame which must access a hardware register to specify the size
    of frames produced.

---
+++ OpenEphys.Onix/OpenEphys.Onix/Test0DataFrame.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace OpenEphys.Onix
+{
+    public class Test0DataFrame
+    {
+        public unsafe Test0DataFrame(oni.Frame frame, int dummyWords)
+        {
+            Clock = frame.Clock;
+            var payload = (Test0Payload*)frame.Data.ToPointer();
+            HubClock = payload->HubClock;
+            Message = payload->Message;
+
+            Dummy = new short[dummyWords];
+            Marshal.Copy(new IntPtr(payload + 10), Dummy, 0, dummyWords);
+        }
+
+        public ulong Clock { get; }
+
+        public ulong HubClock { get; }
+
+        public short Message { get; }
+
+        public short[] Dummy { get; }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct Test0Payload
+    {
+        public ulong HubClock;
+        public short Message;
+    }
+}
