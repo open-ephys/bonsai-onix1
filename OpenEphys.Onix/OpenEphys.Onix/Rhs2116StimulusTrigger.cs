e37d94c60b0ef68c1bcababfc9e6b2f82240d0e9
jonnew <jpn@open-ephys.org>
2024-03-31 22:18:37 +0100

    Finish headstage RHS2116 support
    - Add stimulus trigger device to headstage
    - Add stimulus sequence definition to RHS2116 configuration
    - This will require GUI support for configuring the stimulus waveform.
    - Tested with hardcoded stimulus parameters and it works great.

---
+++ OpenEphys.Onix/OpenEphys.Onix/Rhs2116StimulusTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using Bonsai;
+
+namespace OpenEphys.Onix
+{
+    public class Rhs2116StimulusTrigger : Sink<bool>
+    {
+        [TypeConverter(typeof(Rhs2116Trigger.NameConverter))]
+        public string DeviceName { get; set; }
+
+        public override IObservable<bool> Process(IObservable<bool> source)
+        {
+            return Observable.Using(
+                () => DeviceManager.ReserveDevice(DeviceName),
+                disposable => disposable.Subject.SelectMany(deviceInfo =>
+                {
+                    var device = deviceInfo.GetDeviceContext(typeof(Rhs2116Trigger));
+                    return source.Do(t => device.WriteRegister(Rhs2116Trigger.TRIGGER, t ? 1u : 0u));
+                }));
+        }
+    }
+}
