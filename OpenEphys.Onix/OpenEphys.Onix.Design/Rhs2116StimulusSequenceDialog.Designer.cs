5c4dab4e20b2aac5f195682fe65fe12152fd8164
bparks13 <bap@open-ephys.org>
2024-04-17 16:19:43 -0400

    Add Documentation Link and Cancel Button

--- OpenEphys.Onix/OpenEphys.Onix.Design/Rhs2116StimulusSequenceDialog.Designer.cs
+++ OpenEphys.Onix/OpenEphys.Onix.Design/Rhs2116StimulusSequenceDialog.Designer.cs
@@ -28,30 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button_OK = new System.Windows.Forms.Button();
+            this.button_Cancel = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.linkLabel_Documentation = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(0, 41);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.propertyGrid1);
+            this.splitContainer1.Size = new System.Drawing.Size(1440, 464);
+            this.splitContainer1.SplitterDistance = 1113;
+            this.splitContainer1.TabIndex = 0;
             // 
             // propertyGrid1
             // 
+            this.propertyGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.propertyGrid1.Location = new System.Drawing.Point(0, 0);
             this.propertyGrid1.Name = "propertyGrid1";
-            this.propertyGrid1.Size = new System.Drawing.Size(281, 438);
+            this.propertyGrid1.Size = new System.Drawing.Size(323, 464);
             this.propertyGrid1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button_OK);
+            this.panel1.Controls.Add(this.button_Cancel);
+            this.panel1.Location = new System.Drawing.Point(0, 507);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1437, 46);
+            this.panel1.TabIndex = 2;
+            // 
+            // button_OK
+            // 
+            this.button_OK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_OK.Location = new System.Drawing.Point(1194, 4);
+            this.button_OK.Name = "button_OK";
+            this.button_OK.Size = new System.Drawing.Size(116, 39);
+            this.button_OK.TabIndex = 1;
+            this.button_OK.Text = "OK";
+            this.button_OK.UseVisualStyleBackColor = true;
+            // 
+            // button_Cancel
+            // 
+            this.button_Cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Cancel.Location = new System.Drawing.Point(1316, 4);
+            this.button_Cancel.Name = "button_Cancel";
+            this.button_Cancel.Size = new System.Drawing.Size(116, 39);
+            this.button_Cancel.TabIndex = 0;
+            this.button_Cancel.Text = "Cancel";
+            this.button_Cancel.UseVisualStyleBackColor = true;
+            this.button_Cancel.Click += new System.EventHandler(this.button_Cancel_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 556);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1440, 32);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(179, 25);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1440, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // linkLabel_Documentation
+            // 
+            this.linkLabel_Documentation.AutoSize = true;
+            this.linkLabel_Documentation.Location = new System.Drawing.Point(1313, 4);
+            this.linkLabel_Documentation.Name = "linkLabel_Documentation";
+            this.linkLabel_Documentation.Size = new System.Drawing.Size(118, 20);
+            this.linkLabel_Documentation.TabIndex = 4;
+            this.linkLabel_Documentation.TabStop = true;
+            this.linkLabel_Documentation.Text = "Documentation";
+            this.linkLabel_Documentation.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel_Documentation_LinkClicked);
             // 
             // Rhs2116StimulusSequenceDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 440);
-            this.Controls.Add(this.propertyGrid1);
+            this.ClientSize = new System.Drawing.Size(1440, 588);
+            this.Controls.Add(this.linkLabel_Documentation);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "Rhs2116StimulusSequenceDialog";
             this.Text = "Rhs2116StimulusSequenceDialog";
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button button_OK;
+        private System.Windows.Forms.Button button_Cancel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.LinkLabel linkLabel_Documentation;
     }
 }
--- OpenEphys.Onix/OpenEphys.Onix.Design/Rhs2116StimulusSequenceDialog.cs
+++ OpenEphys.Onix/OpenEphys.Onix.Design/Rhs2116StimulusSequenceDialog.cs
@@ -15,5 +15,23 @@ namespace OpenEphys.Onix.Design
 
             propertyGrid1.SelectedObject = Sequence;
         }
+
+        private void linkLabel_Documentation_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("https://open-ephys.github.io/onix-docs/Software%20Guide/Bonsai.ONIX/Nodes/RHS2116TriggerDevice.html");
+            }
+            catch 
+            {
+                MessageBox.Show("Unable to open documentation link.");
+            }
+        }
+
+        private void button_Cancel_Click(object sender, EventArgs e)
+        {
+            //TODO: Clean everything up here, don't commit anything to the device, close the window
+            Close();
+        }
     }
 }
