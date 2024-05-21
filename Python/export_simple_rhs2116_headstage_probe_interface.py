3daa94ad510b4be4242564c430cee5ff0af766c9
bparks13 <bap@open-ephys.org>
2024-05-21 15:23:37 -0400

    Clean up Python code Duplicate contactIds to show they are not guaranteed to be unique

--- Python/export_simple_rhs2116_headstage_probe_interface.py
+++ Python/export_simple_rhs2116_headstage_probe_interface.py
@@ -12,8 +12,8 @@ from probeinterface.plotting import plot_probe, plot_probe_group
 
 num_channels = 16
 positions = np.zeros((num_channels, 2))
-contact_idsA = [ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15]
-device_idsA =  [ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15]
+contact_ids = [ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15]
+device_ids =  [ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15]
 
 for i in range(num_channels):
     x = i + 1
@@ -21,25 +21,25 @@ for i in range(num_channels):
     positions[i] = x, y
 
 probeA = Probe(ndim=2, si_units='mm')
-polygonA = [(0.5, 2.5), (num_channels + 0.5, 2.5), (num_channels + 0.5, 3.5), (0.5, 3.5), (0.5, 2.5)]
+polygon = [(0.5, 2.5), (num_channels + 0.5, 2.5), (num_channels + 0.5, 3.5), (0.5, 3.5), (0.5, 2.5)]
 
 probeA.set_contacts(positions=positions, shapes='circle', shape_params={'radius': 0.3})
-probeA.set_contact_ids(contact_ids=contact_idsA)
-probeA.set_device_channel_indices(device_idsA)
-probeA.set_planar_contour(polygonA)
+probeA.set_contact_ids(contact_ids=contact_ids)
+probeA.set_device_channel_indices(device_ids)
+probeA.set_planar_contour(polygon)
 
 positions[:, 1] -= 2
-polygonB = [(0.5, 0.5), (num_channels + 0.5, 0.5), (num_channels + 0.5, 1.5), (0.5, 1.5), (0.5, 0.5)]
+polygon = [(0.5, 0.5), (num_channels + 0.5, 0.5), (num_channels + 0.5, 1.5), (0.5, 1.5), (0.5, 0.5)]
 
-contact_idsB = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
-device_idsB =  [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+contact_ids = [ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15]
+device_ids =  [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
 
 probeB = Probe(ndim=2, si_units='mm')
 
 probeB.set_contacts(positions=positions, shapes='circle', shape_params={'radius': 0.3})
-probeB.set_planar_contour(polygonB)
-probeB.set_contact_ids(contact_ids=contact_idsB)
-probeB.set_device_channel_indices(device_idsB)
+probeB.set_planar_contour(polygon)
+probeB.set_contact_ids(contact_ids=contact_ids)
+probeB.set_device_channel_indices(device_ids)
 
 probegroup = ProbeGroup()
 probegroup.add_probe(probeA)
