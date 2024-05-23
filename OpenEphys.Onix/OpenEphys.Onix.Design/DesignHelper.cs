0131a5cd3971224c605606daa04197e15ba1efd6
bparks13 <bap@open-ephys.org>
2024-05-23 16:37:45 -0400

    Add static method to get all children from Form

--- OpenEphys.Onix/OpenEphys.Onix.Design/DesignHelper.cs
+++ OpenEphys.Onix/OpenEphys.Onix.Design/DesignHelper.cs
@@ -1,9 +1,12 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
 using Newtonsoft.Json;
 
 namespace OpenEphys.Onix.Design
 {
-    internal class DesignHelper
+    public static class DesignHelper
     {
         public static T DeserializeString<T>(string channelLayout)
         {
@@ -15,6 +18,20 @@ namespace OpenEphys.Onix.Design
             var stringJson = JsonConvert.SerializeObject(_object);
 
             File.WriteAllText(filepath, stringJson);
+        }
+
+        public static IEnumerable<Control> GetAllChildren(this Control root)
+        {
+            var stack = new Stack<Control>();
+            stack.Push(root);
+
+            while (stack.Any())
+            {
+                var next = stack.Pop();
+                foreach (Control child in next.Controls)
+                    stack.Push(child);
+                yield return next;
+            }
         }
     }
 }
