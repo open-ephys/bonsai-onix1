5ed0c9e036164042270339aaf63811bbeaacb5a4
jonnew <jpn@open-ephys.org>
2024-08-20 13:44:53 -0400

    Finish missing XML comments for neuropixels gain files
    - I only updated the description for one of the files in both the
      Neuropixles2.0e and 2.0ebeta configuration operators

--- OpenEphys.Onix1/ConfigureNeuropixelsV2eBeta.cs
+++ OpenEphys.Onix1/ConfigureNeuropixelsV2eBeta.cs
@@ -57,8 +57,8 @@ namespace OpenEphys.Onix1
         /// across probes. Therefore, using the correct gain calibration file is mandatory to create standardized recordings.
         /// </para>
         /// <para>
-        /// Calibration files are probe-specific and not interchangeable across probes. Calibration files must contain the 
-        /// serial number of the corresponding probe on their first line of text. If you have lost track of a calibration 
+        /// Calibration files are probe-specific and not interchangeable across probes. Calibration files must contain the
+        /// serial number of the corresponding probe on their first line of text. If you have lost track of a calibration
         /// file for your probe, email IMEC at neuropixels.info@imec.be with the probe serial number to retrieve a new copy.
         /// </para>
         /// </remarks>
@@ -78,11 +78,16 @@ namespace OpenEphys.Onix1
         /// Gets or sets the path to the gain calibration file for Probe B.
         /// </summary>
         /// <remarks>
-        /// Each probe is linked to a gain calibration file that contains a gain adjustments determined by IMEC during
+        /// <para>
+        /// Each probe is linked to a gain calibration file that contains gain adjustments determined by IMEC during
         /// factory testing. Electrode voltages are scaled using these values to ensure they can be accurately compared
         /// across probes. Therefore, using the correct gain calibration file is mandatory to create standardized recordings.
-        /// If you have lost track of the gain calibration file for your probe, you can email IMEC at neuropixels.info@imec.be
-        /// with the probe serial number to retrieve a new copy.
+        /// </para>
+        /// <para>
+        /// Calibration files are probe-specific and not interchangeable across probes. Calibration files must contain the
+        /// serial number of the corresponding probe on their first line of text. If you have lost track of a calibration
+        /// file for your probe, email IMEC at neuropixels.info@imec.be with the probe serial number to retrieve a new copy.
+        /// </para>
         /// </remarks>
         [FileNameFilter("Gain calibration files (*_gainCalValues.csv)|*_gainCalValues.csv")]
         [Description("Path to the gain calibration file for probe B.")]
