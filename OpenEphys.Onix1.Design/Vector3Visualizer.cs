59a8bbac03ada85e8165561b508a13f1aa85e927
jonnew <jpn@open-ephys.org>
2024-08-21 11:35:50 -0400

    Add visualizers for Quaternion and Vector3
    - Both of these System.Numerics types are used data frames
      (Bno055DataFrame and TS4231V1PositionDataFrame)

---
+++ OpenEphys.Onix1.Design/Vector3Visualizer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Bonsai;
+using Bonsai.Design.Visualizers;
+using System.Numerics;
+using Bonsai.Vision.Design;
+
+[assembly: TypeVisualizer(typeof(Vector3Visualizer), Target = typeof(Vector3))]
+
+namespace Bonsai.Vision.Design
+{
+    /// <summary>
+    /// Provides a type visualizer that displays a sequence of <see cref="Vector3"/>
+    /// values as a time series.
+    /// </summary>
+    public class Vector3Visualizer : TimeSeriesVisualizer
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Vector3Visualizer"/> class.
+        /// </summary>
+        public Vector3Visualizer()
+            : base(numSeries: 3)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override void Show(object value)
+        {
+            var v = (Vector3)value;
+            AddValue(DateTime.Now, v.X, v.Y, v.Z);
+        }
+    }
+}
