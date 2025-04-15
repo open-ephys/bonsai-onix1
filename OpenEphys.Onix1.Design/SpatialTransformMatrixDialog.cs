ab4fee9baf89b7e0afe7842f723466b77bef6d64
cjsha <cs@open-ephys.org>
2025-04-15 16:33:24 -0400

    Add operator to spatial transform ts4231 data

---
+++ OpenEphys.Onix1.Design/SpatialTransformMatrixDialog.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Windows.Forms;
+using System.Reactive.Linq;
+
+namespace OpenEphys.Onix1.Design
+{
+    public partial class SpatialTransformMatrixDialog : Form
+    {
+        private const byte NumMeasurements = 100;
+
+        private bool[] InputsValid = { false, false, false, false, false, false, false, false };
+        
+        private IObservable<Tuple<int, Vector3>> PositionDataSource;
+
+        private Vector3[] TS4231Coordinates = { Vector3.Zero, Vector3.Zero, Vector3.Zero, Vector3.Zero };
+
+        internal Matrix4x4 SpatialTransform { get; private set; }
+
+        internal bool ApplySpatialTransform { get; private set; }
+
+        internal SpatialTransformMatrixDialog(IObservable<Tuple<int, Vector3>> positionDataSource)
+        {
+            InitializeComponent();
+            PositionDataSource = positionDataSource;
+        }
+
+        private bool CheckInputValidity(string userInput)
+        {
+            string[] serInputSplit = userInput.Split(',');
+            if (serInputSplit.Length != 3)
+            {
+                return false;
+            }
+            return serInputSplit.All(floatCandidate => float.TryParse(floatCandidate, out _));
+        }
+
+        private void DisableButtons() 
+        {
+            buttonMeasure0.Enabled = false;
+            buttonMeasure1.Enabled = false;
+            buttonMeasure2.Enabled = false;
+            buttonMeasure3.Enabled = false;
+            buttonCalculate.Enabled = false;
+        }
+
+        private void EnableButtons() 
+        {
+            buttonMeasure0.Invoke((Action)delegate
+            {
+                buttonMeasure0.Enabled = true;
+            });
+            buttonMeasure1.Invoke((Action)delegate
+            {
+                buttonMeasure1.Enabled = true;
+            });
+            buttonMeasure2.Invoke((Action)delegate
+            {
+                buttonMeasure2.Enabled = true;
+            });
+            buttonMeasure3.Invoke((Action)delegate
+            {
+                buttonMeasure3.Enabled = true;
+            });
+            buttonCalculate.Invoke((Action)delegate
+            {
+                buttonCalculate.Enabled = InputsValid.All(inputValid => inputValid);
+            });
+        }
+
+        private void buttonMeasure_Click(byte coordinate)
+        {
+            TextBox[] ts4231TextBoxes = { textBoxTS4231Coordinate0, textBoxTS4231Coordinate1, textBoxTS4231Coordinate2, textBoxTS4231Coordinate3 };
+
+            textBoxStatus.AppendText(string.Format("Measuring coordinate {0}...", coordinate) + Environment.NewLine);
+
+            DisableButtons();
+
+            var sharedPositionDataGroups = PositionDataSource.Take(NumMeasurements)
+                .GroupBy(dataFrame => dataFrame.Item1, dataFrame => dataFrame.Item2)
+                .Publish();
+
+            sharedPositionDataGroups
+                .SelectMany(group => group.Count().Select(count => new { index = group.Key, measurementCount = count }))
+                .Finally(() =>
+                {
+                    textBoxStatus.Invoke((Action)delegate
+                    {
+                        textBoxStatus.AppendText(string.Format("Measurements at coordinate {0} complete.", coordinate) 
+                            + Environment.NewLine + Environment.NewLine + "Awaiting user input..." + Environment.NewLine);
+                    });
+                    EnableButtons();
+                })
+                .Subscribe(sensor =>
+                {
+                    textBoxStatus.Invoke((Action)delegate
+                    {
+                        textBoxStatus.AppendText(string.Format("{1} measurements from sensor {0}.", sensor.index, sensor.measurementCount) + Environment.NewLine);
+                    });
+                });
+
+            sharedPositionDataGroups
+                .Merge()
+                .Aggregate(
+                    new Vector3(0, 0, 0),
+                    (acc, current) => acc + current,
+                    acc =>
+                    {
+                        TS4231Coordinates[coordinate] = acc / NumMeasurements;
+                        ts4231TextBoxes[coordinate].Invoke((Action)delegate
+                        {
+                            ts4231TextBoxes[coordinate].Text = string.Format("{0}, {1}, {2}",
+                                TS4231Coordinates[coordinate].X,
+                                TS4231Coordinates[coordinate].Y,
+                                TS4231Coordinates[coordinate].Z);
+                        });
+                        return TS4231Coordinates[coordinate];
+                    })
+                .Subscribe();
+
+            sharedPositionDataGroups.Connect();
+        }
+
+        private void textBoxTS4231Coordinate0_TextChanged(object sender, EventArgs e)
+        {
+            
+            buttonCalculate.Enabled = InputsValid.All(inputValid => inputValid);
+        }
+
+        private void textBoxTS4231Coordinate1_TextChanged(object sender, EventArgs e)
+        {
+            buttonCalculate.Enabled = InputsValid.All(inputValid => inputValid);
+        }
+
+        private void textBoxTS4231Coordinate2_TextChanged(object sender, EventArgs e)
+        {
+            buttonCalculate.Enabled = InputsValid.All(inputValid => inputValid);
+        }
+
+        private void textBoxTS4231Coordinate3_TextChanged(object sender, EventArgs e)
+        {
+            buttonCalculate.Enabled = InputsValid.All(inputValid => inputValid);
+        }
+
+        private void buttonMeasure0_Click(object sender, EventArgs e)
+        {
+            buttonMeasure_Click(0);
+            InputsValid[0] = true;
+        }
+
+        private void buttonMeasure1_Click(object sender, EventArgs e)
+        {
+            buttonMeasure_Click(1);
+            InputsValid[1] = true;
+        }
+
+        private void buttonMeasure2_Click(object sender, EventArgs e)
+        {
+            buttonMeasure_Click(2);
+            InputsValid[2] = true;
+        }
+
+        private void buttonMeasure3_Click(object sender, EventArgs e)
+        {
+            buttonMeasure_Click(3);
+            InputsValid[3] = true;
+        }
+        private void textBoxUserCoordinate0_TextChanged(object sender, EventArgs e)
+        {
+            InputsValid[4] = CheckInputValidity(textBoxUserCoordinate0.Text);
+            buttonCalculate.Enabled = InputsValid.All(inputValid => inputValid);
+        }
+
+        private void textBoxUserCoordinate1_TextChanged(object sender, EventArgs e)
+        {
+            InputsValid[5] = CheckInputValidity(textBoxUserCoordinate1.Text);
+            buttonCalculate.Enabled = InputsValid.All(inputValid => inputValid);
+        }
+
+        private void textBoxUserCoordinate2_TextChanged(object sender, EventArgs e)
+        {
+            InputsValid[6] = CheckInputValidity(textBoxUserCoordinate2.Text);
+            buttonCalculate.Enabled = InputsValid.All(inputValid => inputValid);
+        }
+
+        private void textBoxUserCoordinate3_TextChanged(object sender, EventArgs e)
+        {
+            InputsValid[7] = CheckInputValidity(textBoxUserCoordinate3.Text);
+            buttonCalculate.Enabled = InputsValid.All(inputValid => inputValid);
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            var ts4231V1CoordinatesMatrix = new Matrix4x4(
+                TS4231Coordinates[0].X, TS4231Coordinates[0].Y, TS4231Coordinates[0].Z, 1,
+                TS4231Coordinates[1].X, TS4231Coordinates[1].Y, TS4231Coordinates[1].Y, 1,
+                TS4231Coordinates[2].X, TS4231Coordinates[2].Y, TS4231Coordinates[2].Z, 1,
+                TS4231Coordinates[3].X, TS4231Coordinates[3].Y, TS4231Coordinates[3].Z, 1);
+
+            float[][] userCoordinates = {
+                textBoxUserCoordinate0.Text.Split(',').Select(item => float.Parse(item)).ToArray(),
+                textBoxUserCoordinate1.Text.Split(',').Select(item => float.Parse(item)).ToArray(),
+                textBoxUserCoordinate2.Text.Split(',').Select(item => float.Parse(item)).ToArray(),
+                textBoxUserCoordinate3.Text.Split(',').Select(item => float.Parse(item)).ToArray()};
+
+            var userCoordinatesMatrix = new Matrix4x4(
+                userCoordinates[0][0], userCoordinates[0][1], userCoordinates[0][2], 1,
+                userCoordinates[1][0], userCoordinates[1][1], userCoordinates[1][2], 1,
+                userCoordinates[2][0], userCoordinates[2][1], userCoordinates[2][2], 1,
+                userCoordinates[3][0], userCoordinates[3][1], userCoordinates[3][2], 1);
+
+            Matrix4x4.Invert(ts4231V1CoordinatesMatrix, out var ts4231V1CoordinatesMatrixInverted);
+            SpatialTransform = Matrix4x4.Multiply(ts4231V1CoordinatesMatrixInverted, userCoordinatesMatrix);
+
+            textBoxStatus.AppendText("The spatial transform matrix for the above coordinates is:" + Environment.NewLine);
+            textBoxStatus.AppendText(SpatialTransform.ToString() + Environment.NewLine + Environment.NewLine);
+            textBoxStatus.AppendText("Awaiting user input..." + Environment.NewLine);
+
+            checkBoxApplySpatialTransform.Enabled = true;
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void checkBoxApplySpatialTransform_CheckedChanged(object sender, EventArgs e)
+        {
+            ApplySpatialTransform = checkBoxApplySpatialTransform.Checked;
+        }
+    }
+}
---
+++ OpenEphys.Onix1/SpatialTransform.cs
@@ -0,0 +1,32 @@
+﻿using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Numerics;
+
+namespace OpenEphys.Onix1
+{
+    /// <summary>
+    /// Represents an operator that groups the elements of an observable
+    /// sequence according to the specified key.
+    /// </summary>
+    [DefaultProperty(nameof(SpatialTransformMatrix))]
+    [Description("Groups the elements of an observable sequence according to the specified key.")]
+    public class SpatialTransform : Transform<Tuple<int, Vector3>, Vector3>
+    {
+        /// <summary>
+        /// Gets or sets a value specifying the inner properties used as key for
+        /// each element in the sequence.
+        /// </summary>
+        [Description("Specifies the inner properties used as key for each element of the sequence.")]
+        [Editor("OpenEphys.Onix1.Design.SpatialTransformMatrixEditor, OpenEphys.Onix1.Design", DesignTypes.UITypeEditor)]
+        [TypeConverter(typeof(NumericRecordConverter))]
+        public Matrix4x4 SpatialTransformMatrix { get; set; }
+
+        public override IObservable<Vector3> Process(IObservable<Tuple<int, Vector3>> source)
+        {
+            return source.Select(input => Vector3.Transform(input.Item2, this.SpatialTransformMatrix));
+        }
+    }
+}
