2162dc1335a209946032653d90e456a069565630
cjsha <cs@open-ephys.org>
2025-04-16 16:07:07 -0400

    Address (some of) bparks feedback
    - Consolidate win form event handlers
    - Initialize SpatialTransformMatrix property to indentity matrix
    - Refactor some functions here and there

--- OpenEphys.Onix1/SpatialTransform.cs
+++ OpenEphys.Onix1/SpatialTransform.cs
@@ -12,17 +12,21 @@ namespace OpenEphys.Onix1
     /// sequence according to the specified key.
     /// </summary>
     [DefaultProperty(nameof(SpatialTransformMatrix))]
-    [Description("Groups the elements of an observable sequence according to the specified key.")]
+    [Description("Transforms 3D coordinates from one reference frame to another.")]
     public class SpatialTransform : Transform<Tuple<int, Vector3>, Vector3>
     {
         /// <summary>
         /// Gets or sets a value specifying the inner properties used as key for
         /// each element in the sequence.
         /// </summary>
-        [Description("Specifies the inner properties used as key for each element of the sequence.")]
+        [Description("Spatial Transform Matrix")]
         [Editor("OpenEphys.Onix1.Design.SpatialTransformMatrixEditor, OpenEphys.Onix1.Design", DesignTypes.UITypeEditor)]
         [TypeConverter(typeof(NumericRecordConverter))]
-        public Matrix4x4 SpatialTransformMatrix { get; set; }
+        public Matrix4x4 SpatialTransformMatrix { get; set; } = new Matrix4x4(
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1);
 
         public override IObservable<Vector3> Process(IObservable<Tuple<int, Vector3>> source)
         {
