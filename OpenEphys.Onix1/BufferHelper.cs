34adebc0697d24eaaf6543ac5ba2f68bea1991c6
jonnew <jpn@open-ephys.org>
2025-04-18 12:20:42 -0400

    Map Rhd2164 ephys channels into Intan order
    - Added a BufferHelper.CopyTranspose specialization that takes a channel
      map

--- OpenEphys.Onix1/BufferHelper.cs
+++ OpenEphys.Onix1/BufferHelper.cs
@@ -1,4 +1,5 @@
-﻿using OpenCV.Net;
+﻿using System;
+using OpenCV.Net;
 
 namespace OpenEphys.Onix1
 {
@@ -45,6 +46,33 @@ namespace OpenEphys.Onix1
             var data = new Mat(bufferHeader.Cols, bufferHeader.Rows, scale.Depth, 1);
             CV.Transpose(bufferHeader, transposeBuffer);
             CV.Mul(transposeBuffer, scale, data);
+            return data;
+        }
+
+        public static Mat CopyTranspose<TBuffer>(
+        TBuffer[] buffer,
+        int sampleCount,
+        int channelCount,
+        Depth depth,
+        int[] channelMap)
+        where TBuffer : unmanaged
+        {
+            if (channelMap == null || channelMap.Length != channelCount)
+            {
+                throw new ArgumentException($"{nameof(channelMap)} must contain {nameof(channelCount)} entries", nameof(channelMap));
+            }
+
+            using var bufferHeader = Mat.CreateMatHeader(
+                buffer,
+                sampleCount,
+                channelCount,
+                depth,
+                channels: 1);
+            var data = new Mat(bufferHeader.Cols, bufferHeader.Rows, depth, 1);
+
+            for (int i = 0; i < bufferHeader.Cols; i++)
+                CV.Copy(bufferHeader.GetCol(channelMap[i]), data.GetRow(i));
+
             return data;
         }
     }
