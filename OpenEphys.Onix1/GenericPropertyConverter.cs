c9540a47f88483ebd019d046bda873d1375155f1
bparks13 <bap@open-ephys.org>
2025-06-17 14:06:29 -0400

    Add generic property converter

---
+++ OpenEphys.Onix1/GenericPropertyConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace OpenEphys.Onix1
+{
+    /// <summary>
+    /// Generic property converter that can expand all sub-properties of a given class.
+    /// </summary>
+    internal class GenericPropertyConverter : ExpandableObjectConverter
+    {
+        public override PropertyDescriptorCollection GetProperties(ITypeDescriptorContext context, object value, Attribute[] attributes)
+        {
+            var properties = (from property in base.GetProperties(context, value, attributes).Cast<PropertyDescriptor>()
+                              where !property.IsReadOnly
+                              select property)
+                              .ToArray();
+            return new PropertyDescriptorCollection(properties).Sort(properties.Select(p => p.Name).ToArray());
+        }
+    }
+}
